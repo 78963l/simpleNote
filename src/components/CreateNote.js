46a12a6d76f0519e7c309fcc28cc441fbb88fb4b
Sung Min Kim <78963l@naver.com>
2023-04-20 18:07:03 +0900

    삭제 추가

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -3,18 +3,38 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useRef, useState, useEffect } from "react";
 import useFetch from "../hooks/useFetch";
 
-const Title = styled.input`
+const Container = styled.div`
   width: 250px;
+  justify-content: center;
+  display: flex;
+  margin: 0 auto;
+
+  .Title input {
+  }
+`;
+
+const Title = styled.input`
+  width: 100%;
   height: 25px;
-  margin: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
   border-radius: 5px;
   display: inline;
 `;
 
 const Content = styled.textarea`
-  width: 250px;
+  width: 100%;
   height: 100px;
-  margin: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  display: inline;
+`;
+
+const BottomBtn = styled.p`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
 `;
 
 export default function CreateNote() {
@@ -24,38 +44,38 @@ export default function CreateNote() {
   const titleRef = useRef(null);
   const contentRef = useRef(null);
 
-  const [note, setNote] = useState([]);
   const [state, setState] = useState("");
-  
+
   useEffect(() => {
     if (location.state) {
-      setState("edit")
+      setState("edit");
       const noteInfo = location.state.noteInfo;
       titleRef.current.value = noteInfo.title;
       contentRef.current.value = noteInfo.content;
-      
     } else {
-      setState("create")
-      titleRef.current.value = '';
-      contentRef.current.value = '';
+      setState("create");
+      titleRef.current.value = "";
+      contentRef.current.value = "";
     }
+    titleRef.current.focus();
   }, []);
 
   function onSubmit(e) {
+    const curTitle = titleRef.current.value;
+    const curContent = contentRef.current.value;
+
+    if (!curTitle) {
+      alert("title을 작성해주세요.");
+      titleRef.current.focus();
+      return false;
+    }
+    if (!curContent) {
+      alert("content를 작성해주세요.");
+      contentRef.current.focus();
+      return false;
+    }
+
     e.preventDefault();
-
-    const title = titleRef.current.value;
-    const content = contentRef.current.value;
-
-    if (!title) {
-      return alert("title을 작성해주세요.");
-    }
-    if (!content) {
-      return alert("content를 작성해주세요.");
-    }
-
-    console.log(title);
-    console.log(content);
 
     if (state === "create") {
       fetch(`http://localhost:3001/notes`, {
@@ -64,40 +84,58 @@ export default function CreateNote() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          title,
-          content,
+          title: curTitle,
+          content: curContent,
         }),
       }).then((res) => {
         if (res.ok) {
           alert("note create complete!");
-          navigate('/')
+          navigate("/");
         }
       });
     } else if (state === "edit") {
-      console.log("edit");
-      fetch(`http://localhost:3001/notes`, {
+      fetch(`http://localhost:3001/notes/${location.state.noteInfo.id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({}),
+        body: JSON.stringify({
+          ...location.state.noteInfo,
+          title: curTitle,
+          content: curContent,
+        }),
+      }).then((res) => {
+        if (res.ok) {
+          navigate("/");
+        }
       });
+    } else{
+      return
     }
   }
 
+  function del(){
+    console.log("del!!")
+    fetch(`http://localhost:3001/notes/${location.state.noteInfo.id}`, {
+      method: "DELETE",
+    }).then(res => {
+      if(res.ok){
+        alert("삭제되었습니다.");
+        navigate('/');
+      }
+    })
+  }
+
   return (
-    <div>
+    <Container>
       <form onSubmit={onSubmit}>
-        <p>
-          <Title type="text" placeholder="title" ref={titleRef}></Title>
-        </p>
-        <p>
-          <Content placeholder="content" ref={contentRef}></Content>
-        </p>
-        <p>
+        <Title type="text" placeholder="title" ref={titleRef}></Title>
+        <Content placeholder="content" ref={contentRef}></Content>
+        <BottomBtn>
+          {state === "edit" ? <input type="button" value={"del"} onClick={del}></input> : ""}
           <button>{state}</button>
-        </p>
+        </BottomBtn>
       </form>
-    </div>
+    </Container>
   );
 }
